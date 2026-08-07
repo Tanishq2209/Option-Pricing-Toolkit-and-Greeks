eef809f605243302967cca96f1d85240ca779617
Tanishq Gupta <gu.tanishq@gmail.com>
2026-08-07 23:18:57 +0200

    Refactor BlackScholesModel for improved option pricing
    Refactor Black-Scholes model to use class initialization and provide default parameters for pricing options.

--- Models/Black Scholes Model.py
+++ Models/Black Scholes Model.py
@@ -2,13 +2,29 @@ import numpy as np
 from scipy.stats import norm
 
 class BlackScholesModel:
-    def european_BS(t, St, K, T, r, sigma, option='call'):
+    # BS model for European option pricing
+    def __init__(self, S0=100, r=0.05, sigma=0.2, T=1, K=100):
+         self.S0 = S0
+         self.r = r 
+         self.sigma = sigma 
+         self.T = T
+         self.K = K
+
+    def price(self, t=0, St=None, K=None, T=None, r=None, sigma = None, option='call'):
+         # Implement if parameters not provided
+         St = St if St is not None else self.S0
+         K = K if K is not None else self.K
+         r = r if r is not None else self.r
+         T = T if T is not None else self.T
+         sigma = sigma if sigma is not None else self.sigma
+
          d1 = ((np.log(St / K) + (r + 0.5 * sigma**2) * (T - t))) / (sigma * np.sqrt(T - t))
          d2 = d1 - sigma * np.sqrt(T - t)
     
-         if option=='call':
-              return St * norm.cdf(d1) - K * np.exp(-r * (T - t)) * norm.cdf(d2)
+         if option == 'call':
+              price = St * norm.cdf(d1) - K * np.exp(-r * (T - t)) * norm.cdf(d2)
          elif option=='put':
-              return K * np.exp(-r * (T - t)) * norm.cdf(-d2) - St * norm.cdf(-d1)
+              price = K * np.exp(-r * (T - t)) * norm.cdf(-d2) - St * norm.cdf(-d1)
          else:
               raise ValueError("Option type must be either call or put")
+
